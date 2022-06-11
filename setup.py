edc3a7968af9bbcb06b96c6c8ef94076c1a455a7
bpine004 <bpine004@gmail.com>
2022-06-11 16:36:16 +0900

    update project configuration files

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='Python Projcet Template',
-    version='0.0.1',
-    description='Python Project Template',
-    author='your name',
-    author_email='your email address',
-    url='your url',
-    license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
 )
